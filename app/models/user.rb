2ebb91264042fc4a000f6725cab6138d6cccffd5
Damien Joubert <damienusvirfortis@gmail.com>
2026-01-27 19:48:22 +0900

    add user devise user model and views

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
+
+  has_many :chats
+  has_many :messages, through: :chats #maybe unnecessary
+end
