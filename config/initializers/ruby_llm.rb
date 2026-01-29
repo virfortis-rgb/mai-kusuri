b9afc1a89c52c71f55c9820d91069d65cf172dc7
Damien Joubert <damienusvirfortis@gmail.com>
2026-01-29 20:30:04 +0900

    add llm azure token

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -1,3 +1,4 @@
 RubyLLM.configure do |config|
-  config.open_api_key = ENV["OPENAI_API_KEY"]
+  config.openai_api_key = ENV["GITHUB_TOKEN"]
+  config.openai_api_base = "https://models.inference.ai.azure.com"
 end
