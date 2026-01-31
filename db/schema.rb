7a19dd2f04df2724d3b96c3f7a41fb09c55a6615
Mahmud Md Fuad <102977353+fuadaman@users.noreply.github.com>
2026-01-31 14:14:02 +0900

    Added the image feature and fetch more data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_01_27_132326) do
+ActiveRecord::Schema[7.1].define(version: 2026_01_31_120000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.1].define(version: 2026_01_27_132326) do
     t.text "ingredients"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
   end
 
   create_table "messages", force: :cascade do |t|
