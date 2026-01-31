cc66f006901198c577ba42f316deaa1fabff0206
Damien Joubert <damienusvirfortis@gmail.com>
2026-01-31 16:58:54 +0900

    configure ruby_llm

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -1,4 +1,6 @@
 RubyLLM.configure do |config|
   config.openai_api_key = ENV["GITHUB_TOKEN"]
-  config.openai_api_base = "https://models.inference.ai.azure.com"
+  # config.openai_api_base = "https://models.github.ai/inference"
+  config.openai_api_base = "https://api.groq.com/openai/v1"
+  config.default_model = "openai/gpt-oss-20b" # for groq
 end
