f15d1c7f76a3acb62336607e5f39f2516d121730
Damien Joubert <damienusvirfortis@gmail.com>
2026-02-07 12:01:59 +0900

    configure Gemini default model

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -1,11 +1,15 @@
 RubyLLM.configure do |config|
   config.openai_api_key = ENV.fetch('GITHUB_TOKEN', nil)
   config.openai_api_base = "https://models.github.ai/inference"
+  config.default_model = "gpt-4.1-nano"
+
+  # Groq
   # config.openai_api_base = "https://api.groq.com/openai/v1"
   # config.default_model = "openai/gpt-oss-20b" # for groq
 
-  # config.gemini_api_key = ENV.fetch("GEMINI_KEY")
+  # Gemini
+  config.gemini_api_key = ENV.fetch("GEMINI_KEY")
 
-  # ollama
+  # Ollama
   config.ollama_api_base = 'http://localhost:11434/v1'
 end
